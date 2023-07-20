42fa417b04eded81a258dd851f17fb65737c13a4
Paula Morales <paulamorales6729@gmail.com>
2023-07-19 21:47:52 -0400

    Added additional comments

--- lab6.py
+++ lab6.py
@@ -32,7 +32,8 @@ def main():
         if option == 1:
             password = input('Please enter your password to encode: ')
             encoded_password = encode(password)
-            print('Your password has been encoded and stored!')
+            print('Your password has been encoded and stored!\n')
+        # Paula Morales Decoder
         elif option == 2:
             decoded_password = decode(encoded_password)
             print(f'The encoded password is {encoded_password}, and the original password is {decoded_password}.\n')
