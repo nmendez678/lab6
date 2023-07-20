97090554bb85d28c83754bdca57044d782b880c7
nmendez678 <nmendez678@gmail.com>
2023-07-19 20:23:07 -0400

    fixed logic

--- lab6.py
+++ lab6.py
@@ -1,4 +1,12 @@
-# encode password
+# user menu selection
+def menu():
+    print('Menu\n-------------')
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit')
+
+
+# Encodes password
 def encode(password):
     encoded_password = ''
     for digit in password:
@@ -6,23 +14,26 @@ def encode(password):
         encoded_password += encoded_digit
     return encoded_password
 
-# loop
-while True:
-    print('Menu')
-    print('-------------')
-    print('1. Encode')
-    print('2. Decode')
-    print('3. Quit')
 
-    # User menu selection
-    op = int(input('Please enter an option: '))
+# Decodes password
 
-    # Perform selected operation
-    if op == 1:
-        password = input('Please enter your password to encode: ')
-        encoded_password = encode(password)
-        print('Your password has been encoded and stored!\n')
-    elif op == 2:
-        pass
-    elif op == 3:
-        break
+
+def main():
+    while True:
+        menu()
+        option = int(input('\nPlease enter an option: '))
+        
+        if option == 1:
+            password = input('Please enter your password to encode: ')
+            encoded_password = encode(password)
+            print('Your password has been encoded and stored!')
+        elif option == 2:
+            pass
+
+        elif option == 3:
+            break
+        
+
+
+if __name__ == '__main__':
+    main()
